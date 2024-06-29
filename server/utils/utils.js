549b32058db25259859a4d3327b46d049c965f1b
nambininasafidison <safidison23@gmail.com>
2024-06-30 02:49:44 +0300

    Change OS

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -36,7 +36,7 @@ export async function runCommand(command, res) {
 }
 
 export async function verifyToken(req, res, next) {
-  const token = req.headers["authorization"]?.split(" ")[1];
+  const token = req.headers["authorization"].split(" ")[1];
   if (!token) {
     return res.status(403).send("Un token est requis pour l'authentification");
   }
